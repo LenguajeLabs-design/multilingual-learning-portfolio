74bda94eaf72d9d9f4ef3dde20bcf16d3ff1b1a7
Freddie <forozc1@gmail.com>
2026-08-11 20:48:10 +0800

    Configure federicoorozco.co custom domain

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/multilingual-learning-portfolio/",
+  base: "/",
   plugins: [react()],
 });
